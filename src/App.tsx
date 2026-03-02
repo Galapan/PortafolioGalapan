cba14286b054ce2a3d8c7b140eb0133ee10bbb53
Galapan <bastian4le@gmail.com>
2026-03-02 11:40:29 -0600

    Fix: I removed the scroll progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Tech from "./components/Tech";
-import ScrollProgress from "./components/ScrollProgress";
 
 function App() {
   return (
     <div className="bg-black min-h-screen text-white font-sans selection:bg-white selection:text-black">
-      <ScrollProgress />
       <Navbar />
       <main>
         <Hero />
